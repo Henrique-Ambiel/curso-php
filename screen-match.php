1f97d18985a763d8f5fe39ba403fbfdc4da359ec
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-09-17 15:34:58 -0300

    Initial commit

---
+++ screen-match.php
@@ -0,0 +1,12 @@
+<?php //Inicia um código PHP
+
+//Exibe a string no terminal
+echo "Bem vindo(a) ao Screen Match!\n";
+
+//Declaração de variáveis 
+$nomeFilme = "Top Gun - Maverick\n";
+$anoLancamento = 2022;
+$notaFilme = 8.8;
+$incluidoPlano = true;
+
+echo $nomeFilme;
