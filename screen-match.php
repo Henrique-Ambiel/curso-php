76ecc6fddd4116a40489f3424079c1dfc9cdf84a
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-09-18 15:33:28 -0300

    Update screen-match.php

--- screen-match.php
+++ screen-match.php
@@ -6,7 +6,15 @@ echo "Bem vindo(a) ao Screen Match!\n";
 //Declaração de variáveis 
 $nomeFilme = "Top Gun - Maverick\n";
 $anoLancamento = 2022;
-$notaFilme = 8.8;
+$somaNotas = 9;
+
+//Operadores matemáticos
+$somaNotas += 8;
+$somaNotas +=7.5;
+$somaNotas +=6;
+$somaNotas +=5;
+
+$notaFilme = $somaNotas / 5;
 $incluidoPlano = true;
 
-echo $nomeFilme;
+echo $notaFilme;
