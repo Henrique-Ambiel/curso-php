1d5af5f8f4c71852f6d519e92aafe7e0df53c646
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-09-23 13:59:09 -0300

    Update controle-bancario.php

--- controle-bancario.php
+++ controle-bancario.php
@@ -1,4 +1,30 @@
 <?php
-echo "Digite um número: ";
 $numero = (float) fgets(STDIN);
-echo "O número é: $numero\n";
+$saldo = 1000;
+
+//Informações exibidas no início do programa
+echo "************************\n";
+echo "Titular: Henrique Ambiel\n";
+echo "************************\n";
+echo "1. Consultar saldo atual\n";
+echo "2. Sacar\n";
+echo "3. Depositar\n";
+echo "4. Sair\n";
+echo "Digite um número: ";
+
+switch($numero){
+    case 1:
+        echo "Saldo atual: $saldo\n";
+        break;
+    case 2: 
+        echo "Quanto deseja sacar ?: ";
+        break;
+    case 3: 
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default: 
+        break;
+}
