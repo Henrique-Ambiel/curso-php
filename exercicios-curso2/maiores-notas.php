4ad02f859046417b9bd0b9deccf9a1bd81518269
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-10-01 11:43:56 -0300

    Números maiores

---
+++ exercicios-curso2/maiores-notas.php
@@ -0,0 +1,6 @@
+<?php
+
+$notas = [9, 10, 8.5, 9.8, 7, 6];
+rsort($notas); //rsort ordena os elementos do array em ordem decrescente
+
+echo "As três maiores notas são: $notas[0], $notas[1] e $notas[2]";
