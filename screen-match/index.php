55ddae138459c9daf1519ad12463350449d990ec
Henrique <146786119+Henrique-Ambiel@users.noreply.github.com>
2024-10-14 14:59:59 -0300

    Update index.php

--- screen-match/index.php
+++ screen-match/index.php
@@ -2,7 +2,6 @@
 
 require __DIR__ . "/src/Modelo/Filme.php"; //Pega todo o conteúdo do código Modelo e cola aqui
 require __DIR__ . "/src/Modelo/Genero.php"; //Pega todo o conteúdo do código de Genero e cola aqui
-require __DIR__ . "/src/funcoes.php"; //Pega todo o conteúdo do código funcoes e cola aqui
 
 echo "Bem vindo ao ScreenMatch\n"; //Exibe a string no terminal
 
